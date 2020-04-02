bd143db15bbafa4ac76186cdf81b4ec615088726
asdf <migsking@naver.com>
2020-04-02 18:03:39 +0900

    [CREATE] 완주하지 못한 선수

--- hash/완주하지 못한 선수/question.py
+++ hash/완주하지 못한 선수/question.py
@@ -1,2 +1,13 @@
 def solution(participant, completion):
-    return answer
+    for _participant, _completion in zip(sorted(participant), sorted(completion)):
+        if _participant != _completion: return _participant
+
+    return participant[-1]
+
+
+# import collections
+
+
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
