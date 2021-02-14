57442a4afe207be1233511861cec229d4268a559
Minki Son <migsking@naver.com>
2021-02-14 20:22:44 +0900

    Create 10870.py

---
+++ backjun/dp/10870.py
@@ -0,0 +1,13 @@
+# 28776KB 64ms
+
+mem = {0: 0, 1: 1}
+
+def fibonacci(n):
+    if n in mem: return mem[n]
+    else:
+        mem[n] = fibonacci(n - 2) + fibonacci(n - 1)
+        return mem[n]
+
+if __name__ == '__main__':
+    n = int(input())
+    print(fibonacci(n))
